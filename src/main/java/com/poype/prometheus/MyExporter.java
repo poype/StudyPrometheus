4b05a8636ee21ac7f78c86f64c9587a60a426dee
poype <253186926@qq.com>
2023-09-10 16:54:38 +0800

    一行代码对外暴露与 JVM 相关的metric

--- src/main/java/com/poype/prometheus/MyExporter.java
+++ src/main/java/com/poype/prometheus/MyExporter.java
@@ -1,6 +1,7 @@
 package com.poype.prometheus;
 
 import io.prometheus.client.exporter.HTTPServer;
+import io.prometheus.client.hotspot.DefaultExports;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
@@ -10,7 +11,10 @@ public class MyExporter {
     public static void main(String[] args) throws IOException {
 
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        executorService.execute(new HistogramMetricTask());
+//        executorService.execute(new HistogramMetricTask());
+
+        // 对外暴露 JVM 相关的metric
+        DefaultExports.initialize();
 
         // 使用HTTP Server暴露采集的数据
         HTTPServer server = new HTTPServer(8090);
