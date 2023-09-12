b54ffeef92d3e7f867d06680713783ace4b2d20a
poype <253186926@qq.com>
2023-09-12 09:07:22 +0800

    修改 GaugeTask

--- src/main/java/com/poype/prometheus/GaugeMetricTask.java
+++ src/main/java/com/poype/prometheus/GaugeMetricTask.java
@@ -12,27 +12,39 @@ public class GaugeMetricTask implements Runnable {
         Gauge inProgress = Gauge.build()
                                 .name("gauge_metric")
                                 .help("study prometheus gauge metric")
+                                .labelNames("app", "path", "latency")
                                 .register();
 
-        int metricValue = 0;
+        int metricValue1 = 0;
+        int metricValue2 = 0;
         while(true) {
             try {
+                // 延迟3秒
                 TimeUnit.MILLISECONDS.sleep(3000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
 
-            // 模拟请求
-
-            int number = TestUtil.randomNumber();
+            // 生成随机数，给不同标签的metric增加或减少相应的值
+            int number = TestUtil.randomNumber() % 100;
             if (number % 5 == 0) {
-                metricValue -= number;
-                inProgress.dec(number);   // 减少
+                metricValue1 -= number;
+                inProgress.labels("order", "/get", "88").dec(number);   // 减少
+            } else if (number % 5 == 1) {
+                metricValue1 += number;
+                inProgress.labels("order", "/get", "88").inc(number);   // 增加
+            } else if (number % 5 == 2) {
+                metricValue2 -= number;
+                inProgress.labels("pay", "/get", "99").dec(number);     // 减少
+            } else if (number % 5 == 3) {
+                metricValue2 += number;
+                inProgress.labels("pay", "/get", "99").inc(number);     // 增加
             } else {
-                metricValue += number;
-                inProgress.inc(number);   // 增加
+                metricValue2 += number;
+                inProgress.labels("pay", "/get", "99").inc(number);     // 增加
             }
-            System.out.println("Gauge metric value is " + metricValue);
+            System.out.println("Gauge metric1 value is " + metricValue1);
+            System.out.println("Gauge metric2 value is " + metricValue2);
         }
     }
 }
